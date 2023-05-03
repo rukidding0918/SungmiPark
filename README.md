5be29d4a87f0893635f51af06b5f3d6e221e47a6
Hyoung Kyoo Kim <110450164+rukidding0918@users.noreply.github.com>
2023-05-03 14:49:58 +0000

    works css

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # github actions permission error solution
 https://stackoverflow.com/questions/72851548/permission-denied-to-github-actionsbot
+
+# thumbnail creation
+https://superfastpython.com/processpoolexecutor-thumbnail-images/
--- main.py
+++ main.py
@@ -13,12 +13,12 @@ TITLE = os.getenv("TITLE", "SUNGMI PARK")
 IMAGE_ROOT = "./static/images/"
 
 app = flask.Flask(__name__)
+dirs = get_dir_list(IMAGE_ROOT)
 
 
 if __name__ == "__main__":
-    dirs = get_dir_list(IMAGE_ROOT)
-
     with app.app_context():
+        # home
         index_html = render_template(
             'index.html',
             title = TITLE,
@@ -27,15 +27,17 @@ if __name__ == "__main__":
         with open("index.html", "w") as f:
             f.write(index_html)
         
+        # works
         for dir in dirs:
             work_images = get_file_list(IMAGE_ROOT + dir)
             works = []
             for work_image in work_images:
                 works.append({
-                "title": os.getenv("WORK_TITLE", "The little prince"),
-                "description": os.getenv("WORK_DESCRIPTION", "Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest."),
-                "size": os.getenv("WORK_SIZE", "200x300"),
-                "image_url": f"static/images/{dir}/{work_image}",
+                    "title": os.getenv("WORK_TITLE", "The little prince"),
+                    "description": os.getenv("WORK_DESCRIPTION", "Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest."),
+                    "size": os.getenv("WORK_SIZE", "200x300"),
+                    "image_url": f"static/images/{dir}/{work_image}",
+                    "resized_url": f"static/images/{dir}/resized/{work_image}",
                 })
                 work_html = render_template(
                     'works.html',
@@ -46,6 +48,8 @@ if __name__ == "__main__":
                 )
                 with open(f"{dir}.html", "w") as f:
                     f.write(work_html)
+        
+        # profile
         profile_html = render_template(
             'profile.html',
             title = TITLE,
