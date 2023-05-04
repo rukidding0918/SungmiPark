bb256b3ab5c54f277434433f50f4b60b07533ed0
Hyoung Kyoo Kim <110450164+rukidding0918@users.noreply.github.com>
2023-05-04 09:04:48 +0000

    first deploy

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 
 from dotenv import load_dotenv
 from flask import render_template
 import flask
+from PIL import Image
 
 from utils.menus import menu
 from utils.thumbnails import thumbnail
@@ -12,11 +14,36 @@ load_dotenv()
 TITLE = os.getenv("TITLE", "SUNGMI PARK")
 IMAGE_ROOT = "./static/images/"
 
-app = flask.Flask(__name__)
-dirs = get_dir_list(IMAGE_ROOT)
 
+def reset_resized_dir(dir, dest_dir="resized"):
+    resized_dir = os.path.join(dir, dest_dir)
+    if os.path.exists(resized_dir):
+        shutil.rmtree(resized_dir, ignore_errors=True)
+    os.mkdir(resized_dir)
+
+def resize_image(dir, dest_dir, image_name):
+    full_path = os.path.join(dir, image_name)
+    image = Image.open(full_path)
+    image.thumbnail((750, 3000))
+    resized_image_name = os.path.join(dir, dest_dir, image_name)
+    image.save(resized_image_name)
+    return resized_image_name
+
+def resize_images_in_dir(dir, dest_dir="resized"):
+    reset_resized_dir(dir, dest_dir)
+    image_names = get_file_list(dir)
+    resized_image_names = []
+    for image_name in image_names:
+        resized_image_name = resize_image(dir, dest_dir, image_name)
+        resized_image_names.append(resized_image_name)
+    return resized_image_names
+
+
+app = flask.Flask(__name__)
 
 if __name__ == "__main__":
+    dirs = get_dir_list(IMAGE_ROOT)
+
     with app.app_context():
         # home
         index_html = render_template(
@@ -29,15 +56,16 @@ if __name__ == "__main__":
         
         # works
         for dir in dirs:
-            work_images = get_file_list(IMAGE_ROOT + dir)
+            image_dir = os.path.join(IMAGE_ROOT, dir) # ./static/images/2021
+            resized_image_names = resize_images_in_dir(image_dir)
+        
             works = []
-            for work_image in work_images:
+            for work_image in resized_image_names:
                 works.append({
-                    "title": os.getenv("WORK_TITLE", "The little prince"),
-                    "description": os.getenv("WORK_DESCRIPTION", "Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest."),
-                    "size": os.getenv("WORK_SIZE", "200x300"),
-                    "image_url": f"static/images/{dir}/{work_image}",
-                    "resized_url": f"static/images/{dir}/resized/{work_image}",
+                    "title": os.getenv("WORK_TITLE", "title"),
+                    "description": os.getenv("WORK_DESCRIPTION", "description"),
+                    "size": os.getenv("WORK_SIZE", "100호"),
+                    "image_url": work_image,
                 })
                 work_html = render_template(
                     'works.html',
@@ -46,8 +74,8 @@ if __name__ == "__main__":
                     year = dir,
                     works = works,
                 )
-                with open(f"{dir}.html", "w") as f:
-                    f.write(work_html)
+            with open(f"{dir}.html", "w") as f:
+                f.write(work_html)
         
         # profile
         profile_html = render_template(
