4ccd7a7d4708376e0073df0fee0f97eb89ad91e5
Hyoung Kyoo Kim <110450164+rukidding0918@users.noreply.github.com>
2023-05-06 05:41:35 +0000

    haha

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from concurrent.futures import ThreadPoolExecutor
 
 from dotenv import load_dotenv
 from flask import render_template
@@ -12,6 +13,7 @@ from utils.works import get_dir_list, get_file_list
 
 load_dotenv()
 TITLE = os.getenv("TITLE", "SUNGMI PARK")
+EMAIL = os.getenv("EMAIL", "rukidding@khu.ac.kr")
 IMAGE_ROOT = "./static/images/"
 
 
@@ -21,43 +23,65 @@ def reset_resized_dir(dir, dest_dir="resized"):
         shutil.rmtree(resized_dir, ignore_errors=True)
     os.mkdir(resized_dir)
 
-def resize_image(dir, dest_dir, image_name):
-    full_path = os.path.join(dir, image_name)
-    image = Image.open(full_path)
-    image.thumbnail((750, 3000))
+def resize_image(dir, dest_dir, image_name, resize=True):
     resized_image_name = os.path.join(dir, dest_dir, image_name)
-    image.save(resized_image_name)
+
+    if resize:
+        original_image_name = os.path.join(dir, image_name)
+        image = Image.open(original_image_name)
+        image.thumbnail((750, 3000))
+        image.save(resized_image_name)
+
     return resized_image_name
 
-def resize_images_in_dir(dir, dest_dir="resized"):
-    reset_resized_dir(dir, dest_dir)
+def resize_images_in_dir(dir, dest_dir="resized", resize=True):
+    if resize:
+        reset_resized_dir(dir, dest_dir)
     image_names = get_file_list(dir)
     resized_image_names = []
-    for image_name in image_names:
-        resized_image_name = resize_image(dir, dest_dir, image_name)
-        resized_image_names.append(resized_image_name)
+
+    # concurrency
+    pool = ThreadPoolExecutor(max_workers=4)
+    futures = [pool.submit(resize_image, dir, dest_dir, image_name, resize) for image_name in image_names]
+    for future in futures:
+        resized_image_names.append(future.result())
+    
+    # sync
+    # for image_name in image_names:
+    #     resized_image_name = resize_image(dir, dest_dir, image_name)
+    #     resized_image_names.append(resized_image_name)
     return resized_image_names
+
+def render_static_page(template_name, **kwargs):
+    return render_template(
+        template_name,
+        **kwargs
+    )
+
+def write_static_page(template_name, **kwargs):
+    html = render_static_page(template_name, **kwargs)
+    with open(template_name, "w") as f:
+        f.write(html)
 
 
 app = flask.Flask(__name__)
+resize = True
 
 if __name__ == "__main__":
     dirs = get_dir_list(IMAGE_ROOT)
 
     with app.app_context():
-        # home
-        index_html = render_template(
-            'index.html',
-            title = TITLE,
-            menus = dirs,
-        )
-        with open("index.html", "w") as f:
-            f.write(index_html)
-        
+        templates = ["index.html", "profile.html", "notes.html", "articles.html", "contact.html"]
+        for template in templates:
+            if template == "contact.html":
+                write_static_page(template, title=TITLE, email=EMAIL, menus=dirs)
+            else:
+                write_static_page(template, title=TITLE, menus=dirs)
+
         # works
         for dir in dirs:
             image_dir = os.path.join(IMAGE_ROOT, dir) # ./static/images/2021
-            resized_image_names = resize_images_in_dir(image_dir)
+            resized_image_names = resize_images_in_dir(image_dir, resize=resize)
         
             works = []
             for work_image in resized_image_names:
@@ -77,11 +101,3 @@ if __name__ == "__main__":
             with open(f"{dir}.html", "w") as f:
                 f.write(work_html)
         
-        # profile
-        profile_html = render_template(
-            'profile.html',
-            title = TITLE,
-            menus = dirs,
-        )
-        with open("profile.html", "w") as f:
-            f.write(profile_html)
